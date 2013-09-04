b4eab575c35109644f8a22add7589695890c266f
David Pozsar <david.pozsar@megaweb.hu>
2013-09-04 16:42:12 +0200

    add Zork\Form\Element\Search

---
+++ src/Zork/Form/Element/Search.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Zork\Form\Element;
+
+use Zork\Form\Element;
+
+/**
+ * Search
+ *
+ * @author David Pozsar <david.pozsar@megaweb.hu>
+ */
+class Search extends Element
+{
+
+    /**
+     * Seed attributes
+     *
+     * @var array
+     */
+    protected $attributes = array(
+        'type' => 'search',
+    );
+
+}
