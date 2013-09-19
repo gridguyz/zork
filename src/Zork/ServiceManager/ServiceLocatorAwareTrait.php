6efeb2bc75b70300f32e1d65959cfebe9f927da1
David Pozsar <david.pozsar@megaweb.hu>
2013-09-19 16:01:05 +0200

    deprecate Zork's ServiceLocatorAwareTrait

--- src/Zork/ServiceManager/ServiceLocatorAwareTrait.php
+++ src/Zork/ServiceManager/ServiceLocatorAwareTrait.php
@@ -2,38 +2,16 @@
 
 namespace Zork\ServiceManager;
 
-use Zend\ServiceManager\ServiceLocatorInterface;
-
 /**
  * ServiceLocatorTrait
  *
  * @author David Pozsar <david.pozsar@megaweb.hu>
- * @implements \Zend\ServiceManager\ServiceLocatorAwareInterface
+ * @implements  \Zend\ServiceManager\ServiceLocatorAwareInterface
+ * @deprecated
  */
 trait ServiceLocatorAwareTrait
 {
 
-    protected $serviceLocator;
-
-    /**
-     * Set service locator
-     *
-     * @param ServiceLocatorInterface $serviceLocator
-     */
-    public function setServiceLocator( ServiceLocatorInterface $serviceLocator )
-    {
-        $this->serviceLocator = $serviceLocator;
-        return $this;
-    }
-
-    /**
-     * Get service locator
-     *
-     * @return ServiceLocatorInterface
-     */
-    public function getServiceLocator()
-    {
-        return $this->serviceLocator;
-    }
+    use \Zend\ServiceManager\ServiceLocatorAwareTrait;
 
 }
