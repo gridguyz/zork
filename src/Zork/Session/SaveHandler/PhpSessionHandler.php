7b7478a249de9a92e1b9f9d7f6454deca5c440bd
David Pozsar <david.pozsar@megaweb.hu>
2013-10-17 14:27:49 +0200

    rewrite Authentication & Session

---
+++ src/Zork/Session/SaveHandler/PhpSessionHandler.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Zork\Session\SaveHandler;
+
+use SessionHandler;
+use Zend\Session\SaveHandler\SaveHandlerInterface;
+
+/**
+ * PhpSessionHandler save-handler
+ *
+ * @author David Pozsar <david.pozsar@megaweb.hu>
+ * @see    http://php.net/SessionHandler
+ */
+class PhpSessionHandler extends SessionHandler implements SaveHandlerInterface
+{
+}
