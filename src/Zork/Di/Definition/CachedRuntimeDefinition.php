61a9b70cf4a2e72ca7b94e01b536df426d9fa04e
David Pozsar <david.pozsar@megaweb>
2013-12-19 11:56:50 +0100

    cache Di's RuntimeDefinition

---
+++ src/Zork/Di/Definition/CachedRuntimeDefinition.php
@@ -0,0 +1,199 @@
+<?php
+
+namespace Zork\Di\Definition;
+
+use Zend\Cache\Storage\StorageInterface;
+use Zend\Di\Definition\RuntimeDefinition;
+
+/**
+ * CachedRuntimeDefinition
+ *
+ * @author David Pozsar <david.pozsar@megaweb.hu>
+ */
+class CachedRuntimeDefinition extends RuntimeDefinition
+{
+
+    /**
+     * @const int
+     */
+    const DEFAULT_CACHE_MIN_COUNT = 20;
+
+    /**
+     * @const float
+     */
+    const DEFAULT_CACHE_MIN_HITRATE = 0.9;
+
+    /**
+     * @var int
+     */
+    protected $cacheMinCount = self::DEFAULT_CACHE_MIN_COUNT;
+
+    /**
+     * @var float
+     */
+    protected $cacheMinHitrate = self::DEFAULT_CACHE_MIN_HITRATE;
+
+    /**
+     * @var StorageInterface
+     */
+    private $cacheStorage;
+
+    /**
+     * @var bool
+     */
+    private $cacheStorageInitialized;
+
+    /**
+     * @var int
+     */
+    private $cacheStorageCount;
+
+    /**
+     * @return  int
+     */
+    public function getCacheMinCount()
+    {
+        return $this->cacheMinCount;
+    }
+
+    /**
+     * @return  float
+     */
+    public function getCacheMinHitrate()
+    {
+        return $this->cacheMinHitrate;
+    }
+
+    /**
+     * @param   int     $cacheMinCount
+     * @return  CachedRuntimeDefinition
+     */
+    public function setCacheMinCount( $cacheMinCount )
+    {
+        $this->cacheMinCount = abs( (int) $cacheMinCount ) ?: 0;
+        return $this;
+    }
+
+    /**
+     * @param   float   $cacheMinHitrate
+     * @return  CachedRuntimeDefinition
+     */
+    public function setCacheMinHitrate( $cacheMinHitrate )
+    {
+        $this->cacheMinHitrate = abs( (float) $cacheMinHitrate ) ?: 0;
+        return $this;
+    }
+
+    /**
+     * @return  StorageInterface
+     */
+    public function getCacheStorage()
+    {
+        return $this->cacheStorage;
+    }
+
+    /**
+     * @param   StorageInterface    $cacheStorage
+     * @return  CachedRuntimeDefinition
+     */
+    public function setCacheStorage( StorageInterface $cacheStorage )
+    {
+        $this->cacheStorageInitialized = false;
+        $this->cacheStorage = $cacheStorage;
+        return $this;
+    }
+
+    /**
+     * @return  StorageInterface|false
+     */
+    protected function getCacheStorageAvailable()
+    {
+        $storage = $this->getCacheStorage();
+
+        if ( ! $storage )
+        {
+            return false;
+        }
+
+        if ( $this->cacheStorageInitialized )
+        {
+            return $storage;
+        }
+
+        $this->cacheStorageCount = $storage->incrementItem( 'count', 1 ) ?: 1;
+        $this->cacheStorageInitialized = true;
+        return $storage;
+    }
+
+    /**
+     * Get real class name
+     *
+     * @staticvar   array   $map
+     * @param       string  $classLower
+     * @return      string
+     */
+    private function getRealClassName( $classLower )
+    {
+        static $cache = array();
+
+        if ( isset( $cache[$classLower] ) )
+        {
+            return $cache[$classLower];
+        }
+
+        $reflection = new \ReflectionClass( $classLower );
+        return $cache[$classLower] = $reflection->getName();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function processClass( $class )
+    {
+        static $jsonFlags = null;
+        $classLower = strtolower( $class );
+        $className  = $this->getRealClassName( $classLower );
+        $storage    = $this->getCacheStorageAvailable();
+
+        if ( $storage )
+        {
+            $success    = false;
+            $count      = $this->cacheStorageCount;
+            $hit        = $storage->incrementItem( 'hit:' . $classLower, 1 ) ?: 0;
+            $definition = $storage->getItem( 'def:' . $classLower, $success );
+
+            if ( $success )
+            {
+                $this->classes[$className] = json_decode( $definition, true );
+                return;
+            }
+        }
+
+        parent::processClass( $className );
+
+        if ( $storage && $this->getCacheMinCount() <= $count
+                      && $this->getCacheMinHitrate() <= ( $hit / $count ) )
+        {
+            if ( null === $jsonFlags )
+            {
+                $jsonFlags = 0;
+
+                if ( defined( 'JSON_UNESCAPED_SLASHES' ) )
+                {
+                    $jsonFlags |= JSON_UNESCAPED_SLASHES;
+                }
+
+                if ( defined( 'JSON_UNESCAPED_UNICODE' ) )
+                {
+                    $jsonFlags |= JSON_UNESCAPED_UNICODE;
+                }
+            }
+
+            $storage->setItem(
+                'def:' . $classLower,
+                json_encode( $this->classes[$className], $jsonFlags )
+            );
+        }
+    }
+
+}
---
+++ src/Zork/Mvc/Service/DiFactory.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Zork\Mvc\Service;
+
+use Zend\Di\Di;
+use Zend\Di\Config;
+use Zend\Di\DefinitionList;
+use Zend\Cache\StorageFactory;
+use Zend\Mvc\Service\DiFactory as ZendDiFactory;
+use Zend\ServiceManager\ServiceLocatorInterface;
+use Zork\Di\Definition\CachedRuntimeDefinition;
+
+/**
+ * Dependecy injector facory
+ *
+ * @author David Pozsar <david.pozsar@megaweb.hu>
+ */
+class DiFactory implements ZendDiFactory
+{
+
+    /**
+     * @const string
+     */
+    const DEFAULT_CACHE_NAMESPACE = 'Zork\Di\Definition\CachedRuntimeDefinition';
+
+    /**
+     * {@inheritDoc}
+     */
+    public function createService( ServiceLocatorInterface $serviceLocator )
+    {
+        $config = $serviceLocator->get( 'Config' );
+
+        if ( empty( $config['di']['cache'] ) )
+        {
+            return parent::createService( $serviceLocator );
+        }
+
+        $definition = new CachedRuntimeDefinition();
+
+        foreach ( (array) $config['di']['cache'] as $name => $value )
+        {
+            switch ( strtolower( $name ) )
+            {
+                case 'mincount':
+                case 'min_count':
+                case 'cachemincount':
+                case 'cache_min_count':
+                    $definition->setCacheMinCount( $value );
+                    break;
+
+                case 'minhitrate':
+                case 'min_hitrate':
+                case 'cacheminhitrate':
+                case 'cache_min_hitrate':
+                    $definition->setCacheMinHitrate( $value );
+                    break;
+
+                case 'storage':
+                case 'cachestorage':
+                case 'cache_storage':
+                    $value = (array) $value;
+
+                    if ( empty( $value['adapter']['options']['namespace'] ) )
+                    {
+                        $namespace = static::DEFAULT_CACHE_NAMESPACE;
+                    }
+                    else
+                    {
+                        $namespace = $value['adapter']['options']['namespace'];
+                    }
+
+                    $namespace = trim(
+                        preg_replace(
+                            array( '#\\\\+#', '#[^a-z0-9_-]+#' ),
+                            array( '-', '_' ),
+                            strtolower( $namespace )
+                        ),
+                        '-'
+                    );
+
+                    $value['adapter']['options']['namespace'] = $namespace;
+                    $storage = StorageFactory::factory( $value );
+                    $definition->setCacheStorage( $storage );
+                    break;
+            }
+        }
+
+        $di         = new Di( new DefinitionList( $definition ) );
+        $diConfig   = new Config( $config['di'] );
+        $diConfig->configure( $di );
+        return $di;
+    }
+
+}
