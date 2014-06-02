3e621c4ad7cd1ee01a0733c16f6ea66577f87e40
David Pozsar <david.pozsar@megaweb.hu>
2014-06-02 16:43:48 +0200

    form collection view helpers fixed

--- src/Zork/Form/Element/Collection.php
+++ src/Zork/Form/Element/Collection.php
@@ -2,6 +2,7 @@
 
 namespace Zork\Form\Element;
 
+use Zend\Form\FieldsetInterface;
 use Zork\Stdlib\Hydrator\Traversable;
 use Zend\InputFilter\InputProviderInterface;
 use Zork\Form\TranslatorSettingsAwareInterface;
@@ -37,13 +38,23 @@ class Collection extends ElementBase
     protected $count = 0;
 
     /**
-     * @param  null|int|string  $name    Optional name for the element
-     * @param  array            $options Optional options for the element
+     * Set the target element
+     *
+     * @param   ElementInterface|array|\Traversable $elementOrFieldset
+     * @return  Collection
+     * @throws  \Zend\Form\Exception\InvalidArgumentException
      */
-    public function __construct( $name = null, $options = array() )
+    public function setTargetElement( $elementOrFieldset )
     {
-        parent::__construct( $name, $options );
-        $this->hydrator = new Traversable;
+        parent::setTargetElement( $elementOrFieldset );
+
+        if ( $this->targetElement instanceof FieldsetInterface )
+        {
+            $this->targetElement
+                 ->setHydrator( new Traversable );
+        }
+
+        return $this;
     }
 
 }
